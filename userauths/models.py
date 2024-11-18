bb5b436216a5cea61c3e3c3318191a20f1388ab7
masih1999-12 <msh77742@gmail.com>
2024-11-19 02:27:29 +0330

    modify user model

--- userauths/models.py
+++ userauths/models.py
@@ -1,26 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from shortuuid.django_fields import ShortUUIDField
+from django.contrib.auth.models import BaseUserManager
 
-# class UserManager(BaseUserManager):
-#     def create_user(self, phone, password=None, **extra_fields):
-#         if not phone:
-#             raise ValueError('The Phone number must be set')
-#         user = self.model(phone=phone, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('The Phone number must be set')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-#     def create_superuser(self, phone, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-#         if extra_fields.get('is_staff') is not True:
-#             raise ValueError('Superuser must have is_staff=True.')
-#         if extra_fields.get('is_superuser') is not True:
-#             raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
-#         return self.create_user(phone, password, **extra_fields)
+        return self.create_user(phone, password, **extra_fields)
     
 class User(AbstractUser):
     id = ShortUUIDField(
@@ -34,10 +35,11 @@ class User(AbstractUser):
     full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100,unique=True)
     otp = models.CharField(max_length=100, null=True, blank=True)
+    
+    objects = UserManager()
 
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = []
-
     def __str__(self):
         return self.phone
 
