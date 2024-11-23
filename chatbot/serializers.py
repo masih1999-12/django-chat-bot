02da1f594c3deb3394a575b1ed247d46f39c68d5
masih1999-12 <msh77742@gmail.com>
2024-11-23 04:52:32 +0330

    modify chatApiView

--- chatbot/serializers.py
+++ chatbot/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
-
+# from .model_loader import falcon_model
 from .models import Chat
 
-class ChatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Chat
-        fields=('id','request_text','response_text',)
-    def get_fields(self):
-        fields=super().get_fields()
-        if self.context['request'].method == 'POST':
-            fields=dict()
-            fields['request_text'] = serializers.CharField()
-        return fields
-    def create(self, validated_data):
-        chat=Chat.objects.create(request_text=validated_data['request_text'],response_text='hi')
-        return chat
+class ChatSerializer(serializers.Serializer):
+    request_text=serializers.CharField()
--- chatbot/views.py
+++ chatbot/views.py
@@ -3,7 +3,9 @@ from rest_framework.filters import OrderingFilter , SearchFilter
 from rest_framework import  viewsets , mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from rest_framework.response import Response
 
+from .model_loader import run_ollama
 from .models import Chat
 from .serializers import ChatSerializer
 
@@ -13,4 +15,15 @@ class ChatApiView(
                 ):
     queryset=Chat.objects.none()
     serializer_class=ChatSerializer
-    
+    def create(self, request, *args, **kwargs):
+        serializer:ChatSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        request_text = serializer.validated_data['request_text']
+        if not request_text:
+            return Response({"error": "request_text is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        response = run_ollama(request_text)
+        chat=Chat.objects.create(request_text=request_text,response_text=response)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response({'id':chat.id,'response_text':chat.response_text}, status=status.HTTP_201_CREATED, headers=headers)
